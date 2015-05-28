ad6cbccbdec2c5a120de2cb787138107049084d2
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-05-28 15:37:44 +0530

    comited

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -10,35 +10,34 @@ import io.dropwizard.auth.basic.BasicAuthFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class DWGettingStartedApplication
-        extends Application<DWGettingStartedConfiguration> {
+public class DWGettingStartedApplication extends
+		Application<DWGettingStartedConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new DWGettingStartedApplication().run(args);
-    }
-       
-    @Override
-    public String getName() {
-        return "DWGettingStarted";
-    }
-  
-    @Override
-    public void initialize(
-            final Bootstrap<DWGettingStartedConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+	public static void main(final String[] args) throws Exception {
+		new DWGettingStartedApplication().run(args);
+	}
 
-    @Override
-    public void run(final DWGettingStartedConfiguration configuration,
-            final Environment environment) {
-        environment.jersey().register(AuthFactory.binder(
-                new BasicAuthFactory<>(
-                        new GreetingAuthenticator(configuration.getLogin(),
-                                configuration.getPassword()),
-                        "SECURITY REALM",
-                        User.class)));
-        environment.jersey().register(new HelloResource());
-        environment.jersey().register(new SecuredHelloResource());
-    }
+	@Override
+	public String getName() {
+		return "DWGettingStarted";
+	}
+
+	@Override
+	public void initialize(
+			final Bootstrap<DWGettingStartedConfiguration> bootstrap) {
+		// TODO: application initialization
+	}
+
+	@Override
+	public void run(final DWGettingStartedConfiguration configuration,
+			final Environment environment) {
+		environment.jersey().register(
+				AuthFactory.binder(new BasicAuthFactory<>(
+						new GreetingAuthenticator(configuration.getLogin(),
+								configuration.getPassword()), "SECURITY REALM",
+						User.class)));
+		environment.jersey().register(new HelloResource());
+		environment.jersey().register(new SecuredHelloResource());
+	}
 
 }
