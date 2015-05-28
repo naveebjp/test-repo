9d405330fc7c3543183639265af0e4abef1bf9b9
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-05-28 16:09:53 +0530

    commit and push

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -21,7 +21,7 @@ public class DWGettingStartedApplication extends
 	public String getName() {
 		return "DWGettingStarted";
 	}
-	//No agent was running
+	//No agent was running, now it is running
 	@Override
 	public void initialize(
 			final Bootstrap<DWGettingStartedConfiguration> bootstrap) {
