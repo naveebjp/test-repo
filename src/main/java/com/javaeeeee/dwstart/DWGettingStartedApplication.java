e51e981acbf64410181a19cd7526f475682c0491
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-06-09 12:11:34 +0530

    commit succeeded

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -21,7 +21,7 @@ public class DWGettingStartedApplication extends
 	public String getName() {
 		return "DWGettingStarted";
 	}
-	//CodeDeploy Demo 
+	//CodeDeploy Demo succeeded
 	@Override
 	public void initialize(
 			final Bootstrap<DWGettingStartedConfiguration> bootstrap) {
