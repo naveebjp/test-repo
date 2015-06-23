e9289af7d7f08c2ce1e448c7f6ecbbe0dd20ad20
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-06-23 18:13:11 +0530

    commit

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -23,7 +23,7 @@ public class DWGettingStartedApplication extends
 	}
 	//CodeDeploy Demo succeeded.
 	//codeDeploy 
-	//sonar + Docker + testNG need to check
+	//sonar + Docker + 
 	
 	
 	@Override
