6774c1e7274280027a05ba8bd5dd8a8c5a9f1cff
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-07-02 14:54:26 +0530

    commit

--- script/restarttomcat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/application
-nohup java -jar DWGettingStarted-1.0-SNAPSHOT.jar >> log1.log &
-nohup java -jar DWGettingStarted-1.0-SNAPSHOT.jar server config.yml >> log2.log &
-
