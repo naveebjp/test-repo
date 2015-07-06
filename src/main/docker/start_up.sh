9ea44bb60d85eb78289c1dd6bc7bcb1f4a46eff5
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-07-06 19:50:05 +0530

    commit

--- src/main/docker/start_up.sh
+++ src/main/docker/start_up.sh
@@ -1,3 +1,3 @@
 
-nohup java -jar DWGettingStarted-1.0.jar >> log1.log &
-nohup java -jar DWGettingStarted-1.0.jar server /home/ubuntu/config.yml >> log2.log &
+nohup java -jar DWGettingStarted-1.1.jar >> log1.log &
+nohup java -jar DWGettingStarted-1.1.jar server /home/ubuntu/config.yml >> log2.log &
