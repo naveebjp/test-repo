9bb753a1e5782af87dcc2b1ebe3b4a21269a9a7c
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-07-07 10:38:58 +0530

    sdf

--- src/main/docker/start_up.sh
+++ src/main/docker/start_up.sh
@@ -1,3 +1,3 @@
 
-nohup java -jar DWGettingStarted-1.2.jar >> log1.log &
-nohup java -jar DWGettingStarted-1.2.jar server /home/ubuntu/config.yml >> log2.log &
+nohup java -jar DWGettingStarted-1.3.jar >> log1.log &
+nohup java -jar DWGettingStarted-1.3.jar server /home/ubuntu/config.yml >> log2.log &
