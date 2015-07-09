9c5a68521ad7a0703fc496d9397a17639b3b3224
M1018434 <M1018434@A2MD21054.mindtree.com>
2015-07-09 16:23:33 +0530

    :(

--- src/main/docker/start_up.sh
+++ src/main/docker/start_up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nohup java -jar /apps/DWGettingStarted.jar &
-java -jar /apps/DWGettingStarted.jar server /apps/config.yml &
+nohup java -jar /apps/DWGettingStarted.jar server /apps/config.yml &
+service tomcat7 start ;
+tail -f /var/lib/tomcat7/logs/catalina.out
