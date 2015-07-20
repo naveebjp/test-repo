fb605757f1233726828488d1dcb5e326c4edb0a3
naveebjp <naveebjp@gmail.com>
2015-07-20 12:14:54 +0530

    commit v6.0

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -23,7 +23,7 @@ public class DWGettingStartedApplication extends
 	}
 	//CodeDeploy Demo succeeded.
 	//codeDeploy 
-	//sonar + TestNG sonar is running, sonar
+	//Dockerfile path added
 	
 	
 	@Override
